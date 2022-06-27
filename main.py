73010cf4855a087aa04bfe0c87c00b218b34c08a
BAaboe <benjamin.mjaatvedt@gmail.com>
2022-06-27 22:55:26 +0200

    fix

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 
 import os
-import glob
 
-files = glob.glob("")
+files = os.listdir(os.getcwdb())
 
 for file in files:
-    old = file
-    extension = file.split(".")[-1]
-    new = ""  
-    if file.contains("-"):
-        file = file.split("-")[-1]
-    file = file.replace("\n", "")
-    ep = file.split(".")
-    try:
-        ep = int(ep)
-        if str(ep).startswith("0"):
-            new = f"{str(ep)}.{extension}"
-        elif ep < 10:
-            new = f"0{ep}.{extension}"
-        os.rename(old, new)
-    except ValueError:
-        print("dont knwo ep")
+    if os.path(os.path.abspath(file)):
+        old = file
+        extension = file.split(".")[-1]
+        new = ""  
+        if file.contains("-"):
+            file = file.split("-")[-1]
+        file = file.replace("\n", "")
+        ep = file.split(".")
+        try:
+            ep = int(ep)
+            if str(ep).startswith("0"):
+                new = f"{str(ep)}.{extension}"
+            elif ep < 10:
+                new = f"0{ep}.{extension}"
+            os.rename(old, new)
+        except ValueError:
+            print("dont knwo ep")
 
