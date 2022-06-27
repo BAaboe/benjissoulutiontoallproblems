560d3eec0b39cd127f86615ec07c2e9b9dd3a892
BAaboe <benjamin.mjaatvedt@gmail.com>
2022-06-27 23:04:14 +0200

    some fixes

--- main.py
+++ main.py
@@ -3,23 +3,24 @@
 import os
 
 files = os.listdir(os.getcwdb())
-
 for file in files:
-    if os.path(os.path.abspath(file)):
+    file = file.decode()
+    if os.path.isfile(os.path.abspath(file)):
+        print(file)
         old = file
         extension = file.split(".")[-1]
         new = ""  
-        if file.contains("-"):
+        if "-" in file:
             file = file.split("-")[-1]
         file = file.replace("\n", "")
-        ep = file.split(".")
+        ep = file.split(".")[0]
         try:
             ep = int(ep)
             if str(ep).startswith("0"):
                 new = f"{str(ep)}.{extension}"
             elif ep < 10:
                 new = f"0{ep}.{extension}"
-            os.rename(old, new)
+            #os.rename(old, new)
         except ValueError:
             print("dont knwo ep")
 
